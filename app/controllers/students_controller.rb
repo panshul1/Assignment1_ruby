a861f1345bafe0c59adde86f4fff1521fa788435
panshul1 <panshul.kushwaha@gmail.com>
2020-08-28 22:52:44 -0700

    third commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,11 +10,14 @@ class StudentsController < ApplicationController
   def create
     @student=Student.create(params.require(: student).permit(:name, :age))
     if @student.valid?
-      redirect_to student_path
-      
+      redirect_to students_path
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirected_to new_student_path
   end
 
   def show
+    @student =Student.find(params[:id])
   end
 
   def edit
