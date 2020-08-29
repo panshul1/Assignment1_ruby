86ef40f979d63f4a2bb33d8a88ad7bd1fc25ad2e
panshul1 <panshul.kushwaha@gmail.com>
2020-08-29 12:55:24 -0700

    last

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
   root 'students#index'
   resources :students
   resources :enrollments, only: [:new, :create, :destroy]
-  fghnfghnxb
-  gfn
-  fghnfghnxbghb
-  gfhfghbhth
-  fghtfgh
+  
 end
